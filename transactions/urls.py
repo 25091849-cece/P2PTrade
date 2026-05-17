601da8f63914326c44b72877b528138f62c097b0
WengshanLow <wengshanlow@gmail.com>
2026-05-17 18:23:13 +0800

    feat/export per transaction

--- transactions/urls.py
+++ transactions/urls.py
@@ -7,5 +7,6 @@ app_name = 'transactions'
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', views.admin_index, name='admin_index'),
+    path('<int:txn_id>/export/pdf/', views.export_pdf, name='export_pdf'),
 ]
 
