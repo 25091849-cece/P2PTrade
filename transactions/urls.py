1fd1595d2d352711210fc2a73a30fb1efeb678e6
WengshanLow <wengshanlow@gmail.com>
2026-05-24 22:03:37 +0800

    feat/transaction-record-export

--- transactions/urls.py
+++ transactions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', views.admin_index, name='admin_index'),
     path('<int:txn_id>/export/pdf/', views.export_pdf, name='export_pdf'),
+    path('export/summary/', views.export_summary_pdf, name='export_summary'),
 ]
 
